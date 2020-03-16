02f2961b55615cf334fb8083b38f680b1cfe23d0
HADDADSOHAIB <souhaib.haddad@gmail.com>
2020-03-16 19:00:39 +0100

    fix linter issues

--- bin/main.rb
+++ bin/main.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/env ruby
-#! This is from a logic side which will come in the next milestone
+# This is from a logic side which will come in the next milestone
 require_relative '../lib/logic'
 status = Game_status.new
 utils = Game_utils.new
@@ -35,12 +35,12 @@ while status.choosen_option != 2
   puts ''
   puts ''
 
-  #This variable will be moved to logic area later
-  while !status.game_finished
+  # This variable will be moved to logic area later
+  until status.game_finished
     puts ''
     puts ''
 
-    #display is a function in logic file that display either ' ', o or x
+    # display is a function in logic file that display either ' ', o or x
     puts '    -------------'
     puts "    | #{utils.display_move(1, 1)} | #{utils.display_move(2, 1)} | #{utils.display_move(3, 1)} |"
     puts '    -------------'
@@ -52,9 +52,9 @@ while status.choosen_option != 2
     puts ''
 
     valid_move = false
-    while !valid_move
+    until valid_move
 
-      puts 'player #{utils.player_turn == 4 ? 2 : 1} move:'
+      puts "player #{utils.player_turn == 4 ? 2 : 1} move:"
       puts ' 1- position (1, 1)' if utils.position_available?(1, 1)
       puts ' 2- position (1, 2)' if utils.position_available?(1, 2)
       puts ' 3- position (1, 3)' if utils.position_available?(1, 3)
@@ -80,27 +80,27 @@ while status.choosen_option != 2
   end
 
   puts "the winner of this round is player #{status.winner}."
-  puts "Score:"
-  puts "-------------"
+  puts 'Score:'
+  puts '-------------'
   puts "Palyer 1: #{status.score[0]}"
-  puts "-------------"
+  puts '-------------'
   puts "Palyer 2: #{status.score[1]}"
-  puts "-------------"
+  puts '-------------'
 
-  puts ""
-  puts ""
+  puts ''
+  puts ''
 
   while status.choosen_option != 1 && status.choosen_option != 2
-    puts "Options:"
-    puts "1 - Continue playing"
-    puts "2 - Exit the game"
-    puts ""
-    puts "Choose an option (type 1 or 2):"
+    puts 'Options:'
+    puts '1 - Continue playing'
+    puts '2 - Exit the game'
+    puts ''
+    puts 'Choose an option (type 1 or 2):'
     status.update_option(Integer(gets.chomp))
-    puts ""
-    puts ""
-    puts "Incorrect choice, choose from these options" if status.choosen_option != 1 && status.choosen_option != 2
-    puts ""
-    puts ""
+    puts ''
+    puts ''
+    puts 'Incorrect choice, choose from these options' if status.choosen_option != 1 && status.choosen_option != 2
+    puts ''
+    puts ''
   end
 end
