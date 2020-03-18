60c19c34ccabebceea3f08d7fdf5010146a53bfd
HADDADSOHAIB <souhaib.haddad@gmail.com>
2020-03-18 16:21:33 +0100

    test update status ligne 1

--- spec/game_status_specs.rb
+++ spec/game_status_specs.rb
@@ -6,29 +6,39 @@ RSpec.describe GameStatus do
     it "initialize @score to [0, 0]" do
       expect(status.score).to eql([0, 0])
     end
-  end
 
-  describe '#initialize' do
     it "initialize @game_finished to false" do
       expect(status.game_finished).to eql(false)
     end
-  end
 
-  describe '#initialize' do
     it "initialize @winner to 0" do
       expect(status.winner).to eql(0)
     end
-  end
 
-  describe '#initialize' do
     it "initialize @choosen_option to 0" do
       expect(status.choosen_option).to eql(0)
     end
-  end
 
-  describe '#initialize' do
     it "$players_moves variable exist" do
       expect($players_moves).to_not eql(nil)
+    end
+  end
+
+  describe '#update_status' do
+    it "Player 1 win if he fill ligne 1" do
+      $players_moves[0][0] = 1
+      $players_moves[0][1] = 1
+      $players_moves[0][2] = 1
+      status.update_status
+      expect(status.winner).to eql(1)
+    end
+    it "Player 1 win if he fill ligne 1" do
+      $players_moves[0][0] = 1
+      $players_moves[0][1] = 1
+      $players_moves[0][2] = 1
+      score = status.score.dup
+      status.update_status
+      expect(status.score[0]).to eql(score[0] + 1)
     end
   end
 end
